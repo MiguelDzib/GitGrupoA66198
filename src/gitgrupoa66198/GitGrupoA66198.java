3cb4b7a9fdf724ee75a54fd45575193634c19fcb
galbe <galbe@192.168.1.68>
2021-02-25 10:29:38 -0600

    Mi primer commit

---
+++ src/gitgrupoa66198/GitGrupoA66198.java
@@ -0,0 +1,14 @@
+
+package gitgrupoa66198;
+
+public class GitGrupoA66198 {
+
+    public static void main(String[] args) {
+        
+    String nombre = "Miguel Angel Dzib Alayola";
+    System.out.println ("Mi nombre es " + nombre);
+        
+        
+    }
+    
+}
